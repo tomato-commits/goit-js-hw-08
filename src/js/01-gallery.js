bdc3d56d6f485ad282931dfd823deae248735af8
tomato-commits <arthurpeter7@gmail.com>
2021-11-08 20:56:06 +0200

    task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,36 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { galleryItems } from './gallery-items.js';
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+gallery.addEventListener("click", onImageClick);
+createGalleryItems();
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionDelay: 250,
+    captionsData: 'alt',
+});
+
+function createGalleryItems() {
+    const markup = galleryItems
+        .map((image) => {
+            return `
+                <a class="gallery__item" href="${image.original}">
+                    <img
+                        class="gallery__image"
+                        src="${image.preview}" 
+                        alt="${image.description}"
+                    />
+                </a>
+            `;
+         })
+        .join('');
+    
+    gallery.insertAdjacentHTML("beforeend", markup);
+}
+
+function onImageClick(event) {
+    // prevent link from opening
+    event.preventDefault();
+}
