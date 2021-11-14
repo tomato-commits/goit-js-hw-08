22a196baea56adece51ca0b97e51053130f7520b
tomato-commits <arthurpeter7@gmail.com>
2021-11-14 21:35:50 +0200

    task #2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,34 @@
 
+import Player from '@vimeo/player';
+import { throttle } from 'lodash';
+
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+let serializedCurrentTime;
+
+try {
+    const videoPlayerCurrentTime = localStorage.getItem("videoplayer-current-time");
+    serializedCurrentTime = videoPlayerCurrentTime === null ? undefined : JSON.parse(videoPlayerCurrentTime);
+} catch (error) {
+    console.error("Get state error: ", error.message);
+}
+
+if (serializedCurrentTime) {
+    player.setCurrentTime(serializedCurrentTime)
+        .catch((error) => {
+            switch (error.name) {
+                case 'RangeError':
+                    throw new Error('The time was less than 0 or greater than the video’s duration');
+
+                default:
+                    throw new Error('Error with Vimeo player occured');
+            }
+    });
+}
+
+
+const onPlay = ({seconds}) => {
+    localStorage.setItem("videoplayer-current-time", JSON.stringify(seconds));
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
